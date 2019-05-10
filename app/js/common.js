2ee14f0ca06d01288a99b68374a38f89cc76fbd0
konstantin <ykos.web@gmail.com>
2019-05-10 16:15:28 +0300

    header adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,53 @@
 $(function() {
+	var burger = document.getElementById("burger");
+	var navbar = document.getElementById("navbar");
+	var closeBtn = document.getElementById("close");
 
-	// Custom JS
+	closeBtn.addEventListener("click", function(event) {
+		event.preventDefault();
+		navbar.classList.toggle("nav-active");
+	});
+	
+	burger.addEventListener("click", adaptiveMenu);
+
+	function adaptiveMenu(event) {
+		event.preventDefault();
+		navbar.classList.toggle("nav-active");
+	};
+
+	
+  $('.header_content').slick({
+		
+		// autoplaySpeed: 2000,
+		// autoplay: true,
+		prevArrow: '<i class="fas fa-chevron-right"></i>',
+		nextArrow: '<i class="fas fa-chevron-left"></i>',
+		infinite: false,		
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					infinite: true,
+					dots: true,
+					dotsClass: "my-dots"
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					infinite: true,
+					dots: true,
+					dotsClass: "my-dots"
+				}
+			}
+		]
+
+	});
+    
+	
+	
+	
 
 });
